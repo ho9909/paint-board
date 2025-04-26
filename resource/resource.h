15b1727f237759c1f3600c4f3c75e7d9b5ffaccd
Seong In Jo <85017429+ho9909@users.noreply.github.com>
2025-04-26 20:09:05 +0900

    Update and rename resource.h to resource/resource.h

---
+++ resource/resource.h
@@ -0,0 +1,43 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by wewe.rc
+//
+#define IDR_MENU1                       101
+#define IDB_BITMAP1                     102
+#define ID_FILEOPEN                     40001
+#define ID_FILL_GREEN                   40002
+#define ID_NEWFILE                      40027
+#define ID_FILESAVE                     40028
+#define ID_FILEEXIT                     40029
+#define ID_SOLIDLINE                    40030
+#define ID_DASHLINE                     40031
+#define ID_DOTTEDLINE                   40033
+#define ID_CIRCLE                       40034
+#define ID_TRIANGLE                     40035
+#define ID_LINE                         40036
+#define ID_FREELINE                     40037
+#define ID_ERASE                        40038
+#define ID_RECTANGLE                    40039
+#define ID_RED                          40040
+#define ID_BLACK                        40041
+#define ID_BLUE                         40042
+#define ID_GREEN                        40043
+#define ID_PX_1PX                       40044
+#define ID_PX_5PX                       40045
+#define ID_PX_10PX                      40046
+#define ID_FILL_RED                     40047
+#define ID_FILL_BLACK                   40048
+#define ID_FILL_BLUE                    40049
+#define ID_FILL_WHITE                   40050
+#define ID_FILL_NON                     40051
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40052
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
